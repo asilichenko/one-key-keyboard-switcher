d6444a223431e236daad3146ff2a897cf7dc3348
Oleksii Sylichenko <a.silichenko@gmail.com>
2026-04-25 19:47:19 +0300

    add script icon png to ico converter

---
+++ scripts/icon_png_to_ico_converter.py
@@ -0,0 +1,42 @@
+#  Copyright (C) 2026 Oleksii Sylichenko (a.silichenko@gmail.com)
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from pathlib import Path
+from PIL import Image
+
+if __name__ == '__main__':
+    """
+    Конвертує png картинку в ico файл, який містить серію розмірів цієї картинки. 
+    Переглянути можна в GIMP.s"""
+
+    assets_path = Path(__file__).parent.parent.parent / "Assets"
+
+    input_file = assets_path / 'StoreLogo.png'
+    output_file = assets_path / 'icon.ico'
+
+    img = Image.open(input_file)
+
+    img.save(
+        output_file,
+        format="ICO",
+        sizes=[
+            (16, 16),
+            (32, 32),
+            (48, 48),
+            (64, 64),
+            (128, 128),
+            (256, 256),
+        ]
+    )
