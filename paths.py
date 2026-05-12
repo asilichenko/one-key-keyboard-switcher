477c04c22a6d1dc4a6a7bef568ec21872592e145
Oleksii Sylichenko <a.silichenko@gmail.com>
2026-05-12 15:05:12 +0300

    update path.py

--- paths.py
+++ paths.py
@@ -26,22 +26,26 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-# paths.py
 import os
 import sys
 from pathlib import Path
-from constants import APP_NAME, ERR_LOG_FILENAME
+from constants import ERR_LOG_FILENAME, APP_DATA_DIR_MSIX, APP_DATA_DIR
+from onekey_layout_switcher.utils import winapi_utils
 
 
 def get_app_data_folder() -> Path:
     """
     Повертає базову директорію для даних додатка.
-    В MSIX це буде: LocalAppData/Packages/<PackageID>/LocalCache/Local/YourApp
+    В MSIX це буде: %LocalAppData%/Packages/<PackageFamilyName>/LocalCache/Local/YourApp
     """
     local_app_data = os.getenv('LOCALAPPDATA')
-    base_path = Path(local_app_data) / APP_NAME
 
-    return base_path
+    if not local_app_data:
+        raise RuntimeError("%LOCALAPPDATA% environment variable is not set")
+
+    appdata_dir_name = APP_DATA_DIR_MSIX if winapi_utils.is_msix_package() else APP_DATA_DIR
+
+    return Path(local_app_data) / appdata_dir_name
 
 
 def get_err_log_file_path() -> Path:
@@ -53,9 +57,17 @@ def get_err_log_file_path() -> Path:
 
 def get_root_dir() -> str:
     if getattr(sys, 'frozen', False):
-        # if run as EXE (PyInstaller)
+        # if run as executable (PyInstaller)
         application_path: str = os.path.dirname(sys.executable)
     else:
         # if run as script .py
         application_path: str = os.path.dirname(os.path.abspath(__file__))
     return application_path
+
+
+def get_root_module_dir() -> str:
+    return os.path.join(get_root_dir(), "onekey_layout_switcher")
+
+
+def get_assets_dir() -> str:
+    return os.path.join(get_root_module_dir(), "assets")
